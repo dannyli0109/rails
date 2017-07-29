2d1be210a5ae4aead9baf4c9a46a2ed4e4104f1a
Danny Li <dannyli0109@gmail.com>
2017-07-29 14:59:59 +1000

    json response

--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -2,8 +2,37 @@ class Api::QuestionsController < ApplicationController
 
   def finance_show
 
+    request = Typhoeus::Request.new(
+    "https://raap.d61.io/api/v0/domain/demonstration/schema",
+    headers: { 'Content-Type' => "application/json", "Authorization" => "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJlMGQ3YmYwZS1mODhiLTQwZTktOTRjNS05MTU4OGIxNTJmMGYiLCJleHAiOjE1MDM3MDg5NDIsImlhdCI6MTUwMTI4OTc0Mn0.Z-uGwCZbNuFGWe-SLSru1C08fsFlEhUyMavHtqUlzQA" }
+    )
 
-    render json: [{"question": "question1"}]
+    json = {}
+
+    request.on_complete do |response|
+
+
+      if response.success?
+        json = JSON.parse(response.body)
+        # hell yeah
+      elsif response.timed_out?
+        # aw hell no
+        log("got a time out")
+      elsif response.code == 0
+        # Could not get an http response, something's wrong.
+        log(response.return_message)
+      else
+        # Received a non-successful http response.
+        log("HTTP request failed: " + response.code.to_s)
+      end
+    end
+
+
+
+    request.run
+
+
+    render json: json
   end
 
 
